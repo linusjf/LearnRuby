bf398159880fa92b37617f9ab70ec7e666423569
Linus Fernandes <linus.fernandes@gmail.com>
2023-04-30 10:01:43 +0530

    first commit

---
+++ redis/bench.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'redis'
+
+def bench(descr)
+  start = Time.now
+  yield
+  puts "#{descr} #{Time.now - start} seconds"
+end
+
+def without_pipelining
+  r = Redis.new
+  10_000.times do
+    r.ping
+  end
+end
+
+def with_pipelining
+  r = Redis.new
+  r.pipelined do
+    10_000.times do
+      r.ping
+    end
+  end
+end
+
+bench('without pipelining') do
+  without_pipelining
+end
+bench('with pipelining') do
+  with_pipelining
+end
